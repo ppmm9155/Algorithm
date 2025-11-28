9f10ce7cb29e1e27e52068192a67d0dc7a4e8a58
박기준 <62309284+ppmm9155@users.noreply.github.com>
2025-11-28 22:47:42 +0900

    [Bronze V] Title: A+B - 5, Time: 184 ms, Memory: 17972 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A,B;
+		
+		while(true)
+		{
+		    A = sc.nextInt();
+		    B = sc.nextInt();
+		    
+		    
+		    if(!(A==0&&B==0))
+		    {
+		        System.out.println(A+B);
+		        continue;
+		    }else{
+		        break;
+		    }
+		    
+		   
+		       
+		}
+		
+		
+		
+	}
+}
